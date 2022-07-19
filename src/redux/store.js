f09685ea9aaf4622fc492af59d6e5beba58a7df6
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-19 13:03:17 +0600

    build: update store with componse, enhancher & thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducers from "./reducers/";
 
-const store = createStore(
-  reducers,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
